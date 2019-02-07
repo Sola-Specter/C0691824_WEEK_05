633666b4c2799d65f607a8ee2d556988aba1b85e
visualstudio <visualstudio@230-PC05>
2019-02-07 11:53:44 -0500

    more commit

--- C0691824/js/main.js
+++ C0691824/js/main.js
@@ -2,5 +2,17 @@
 
 function myFunction(a, b) {
     sum = a + b;
-    alert("THIS IS THE SUM " + sum);    
+    console.log("The sum a and b is " + sum);
+}
+
+
+var pens = ["red", "orange", "blue", "black", "green","indigo","pink"];   
+function arrays() {   
+    
+    for (i = 0; i <= pens.length - 1; i++) {
+        console.log(pens[i]);
+        alert(pens[i]);
+        
+    }
+    console.log("length of the array is " + pens.length);
 }
