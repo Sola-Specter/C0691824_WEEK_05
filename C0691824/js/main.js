309044b75bae451c8141dc9d5a143b50fe35ecc4
visualstudio <visualstudio@230-PC05>
2019-02-07 10:43:15 -0500

    more code

--- C0691824/js/main.js
+++ C0691824/js/main.js
@@ -1,3 +1,6 @@
 ﻿// Your code here!
-var a = 5, b = 6, sum = a+b;
-alert("THIS IS THE SUM " + sum);               
+
+function myFunction(a, b) {
+    sum = a + b;
+    alert("THIS IS THE SUM " + sum);    
+}
